7a8daa5aeae98ba423a626c93c93964e816b767f
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-26 21:55:19 +0200

    feat: customer controller improvements

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { WithoutId } from '../types/common.types';
-import { CustomerEntity } from '../types/entities/customer.entity';
+import { Customer } from '@prisma/client';
+import { CreateCustomerDto, UpdateCustomerDto } from '../types/dtos/customer.dto';
 import { CustomerServiceInterface } from '../types/service-interfaces/customer.service.interface';
 
 @Controller('customer')
@@ -8,27 +8,27 @@ export class CustomerController {
   constructor(private readonly customerService: CustomerServiceInterface) {}
 
   @Get()
-  getCustomers() {
+  async getCustomers(): Promise<Customer[]> {
     return this.customerService.getCustomers();
   }
 
   @Get(':id')
-  getCustomer(@Param('id') id: string) {
+  async getCustomer(@Param('id') id: string): Promise<Customer> {
     return this.customerService.getCustomerById(id);
   }
 
   @Post()
-  createCustomer(@Body() customer: WithoutId<CustomerEntity>) {
+  async createCustomer(@Body() customer: CreateCustomerDto): Promise<Customer> {
     return this.customerService.createCustomer(customer);
   }
 
   @Patch(':id')
-  updateCustomer(@Param('id') id: string, @Body() customer: CustomerEntity) {
+  async updateCustomer(@Param('id') id: string, @Body() customer: UpdateCustomerDto): Promise<Customer> {
     return this.customerService.updateCustomer(id, customer);
   }
 
   @Delete(':id')
-  deleteCustomer(@Param('id') id: string) {
+  async deleteCustomer(@Param('id') id: string): Promise<void> {
     return this.customerService.deleteCustomer(id);
   }
 }
