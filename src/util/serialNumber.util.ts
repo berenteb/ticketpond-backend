021c4f726f56bd0c3f641e18bdbd78aa815f41a8
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-10-27 17:24:32 +0200

    fix: order serial number month fixed

--- src/util/serialNumber.util.ts
+++ src/util/serialNumber.util.ts
@@ -11,7 +11,7 @@ export function generateSerialNumber(): string {
 export function generateDateBasedSerialNumber(): string {
   const date = new Date();
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const day = date.getDate();
   const characters = '0123456789';
   let randomPart = `${year}.${month}.${day}.`;
