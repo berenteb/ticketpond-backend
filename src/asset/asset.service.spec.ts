e8a5c0f160d52082b74e26a6aa7470b8a7fa7ac3
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-11-22 20:04:46 +0100

    chore: added asset service test

--- src/asset/asset.service.spec.ts
+++ src/asset/asset.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as fs from 'fs';
+import { MemoryStoredFile } from 'nestjs-form-data';
+import * as path from 'path';
 import { AssetService } from './asset.service';
 
-describe('AssetService', () => {
-  let service: AssetService;
+let service: AssetService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AssetService],
-    }).compile();
+const pathName = path.resolve(__dirname, '../../../static/uploads');
 
-    service = module.get<AssetService>(AssetService);
-  });
+jest.mock('fs', () => ({
+  promises: {
+    rm: jest.fn(),
+    writeFile: jest.fn(),
+  },
+}));
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+beforeEach(async () => {
+  const module: TestingModule = await Test.createTestingModule({
+    providers: [AssetService],
+  }).compile();
+
+  service = module.get<AssetService>(AssetService);
+});
+
+it('should call delete fs rm', () => {
+  service.deleteFile('test');
+  expect(fs.promises.rm).toBeCalledWith(path.resolve(pathName, 'test'));
+});
+
+it('should call writeFile with random generated fileName, and return file name', async () => {
+  const fileName = await service.uploadFile({
+    extensionWithSource: {
+      value: 'png',
+    },
+    buffer: Buffer.from('test'),
+  } as unknown as MemoryStoredFile);
+  expect(fs.promises.writeFile).toBeCalledWith(expect.any(String), expect.anything());
+  expect(fileName).toMatch(/[0-9A-Z]{16}\.png/);
 });
